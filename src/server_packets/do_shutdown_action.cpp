0f359e5e3d71383433d91013ed3c1a49f90841fc
ubbelol <ubbelolhf@gmail.com>
2016-08-27 17:49:27 +0200

    add support for shutdown/reboot on windows

--- src/server_packets/do_shutdown_action.cpp
+++ src/server_packets/do_shutdown_action.cpp
@@ -1,6 +1,10 @@
 #include "stdafx.h"
 #include "packets/server_packets.h"
 
+#ifdef WIN32
+#include <powrprof.h>
+#endif
+
 #ifdef __linux__
 #include <sys/reboot.h>
 #endif
@@ -24,7 +28,8 @@ void do_shutdown_action_packet::execute(quasar_client &client) {
 	switch (m_action) {
 		case ShutdownAction::Shutdown:
 #ifdef WIN32
-
+			/* use EWX_FORCE? */
+			ExitWindowsEx(EWX_SHUTDOWN, 0x00000001 /* SHTDN_REASON_MINOR_MAINTENANCE */);
 #elif __linux__
 			if (reboot(RB_POWER_OFF) == -1) {
 				auto response = std::make_shared<set_status_packet>("Failed to shutdown system");
@@ -34,7 +39,7 @@ void do_shutdown_action_packet::execute(quasar_client &client) {
 			break;
 		case ShutdownAction::Restart:
 #ifdef WIN32
-
+			ExitWindowsEx(EWX_REBOOT, 0x00000001 /* SHTDN_REASON_MINOR_MAINTENANCE */);
 #elif __linux__
 			if (reboot(RB_AUTOBOOT) == -1) {
 				auto response = std::make_shared<set_status_packet>("Failed to reboot system");
@@ -44,7 +49,7 @@ void do_shutdown_action_packet::execute(quasar_client &client) {
 			break;
 		case ShutdownAction::Standby:
 #ifdef WIN32
-
+			/* Do nothing for now. We'll probably have to link PowrProf.lib to actually suspend */
 #elif __linux__
 			if (reboot(RB_SW_SUSPEND) == -1) {
 				auto response = std::make_shared<set_status_packet>("Failed to put system in standby");
